8853b97b20d652877c3c1bd26cd729f1c50e9e55
nikita <nikita45454@gmail.com>
2022-12-26 13:41:02 +0300

    feat: добавляет скролинг к невалидному полю

--- src/components/FormGaz1.vue
+++ src/components/FormGaz1.vue
@@ -3,25 +3,27 @@
     <h1 class="h1 width-640">
       Заключение договора на техническое обслуживание и&nbsp;ремонт газовых сетей и оборудования
     </h1>
-    <Form ref="formRef" class="width-480 tw-space-y-2" @submit="onSubmit">
-      <AppSelect rules="required" label="Тип заявителя" name="type" :options="typeOptions" />
-      <AppSelect rules="required" label="Объект" name="object" :options="objectOptions" />
+    <Form ref="formRef" as="div" class="width-480" v-slot="{ validate }">
+      <form class="tw-space-y-2" @submit.prevent="onSubmit(validate)">
+        <AppSelect rules="required" label="Тип заявителя" name="type" :options="typeOptions" />
+        <AppSelect rules="required" label="Объект" name="object" :options="objectOptions" />
 
-      <FieldsClientData />
-      <FieldsMailAddress />
-      <FieldsConfidant />
-      <div class="tw-pt-4 tw-pb-2">
-        <p class=" tw-text-gray-800 tw-leading-tight tw-font-medium tw-text-base">
-          Прошу заключить договор на техническое обслуживание и ремонт газовых сетей и оборудования, расположенного по адресу:
-        </p>
-      </div>
-      <FieldsObjectData />
-      <FieldsNotifications />
-      <FieldsFiles />
+        <FieldsClientData />
+        <FieldsMailAddress />
+        <FieldsConfidant />
+        <div class="tw-pt-4 tw-pb-2">
+          <p class=" tw-text-gray-800 tw-leading-tight tw-font-medium tw-text-base">
+            Прошу заключить договор на техническое обслуживание и ремонт газовых сетей и оборудования, расположенного по адресу:
+          </p>
+        </div>
+        <FieldsObjectData />
+        <FieldsNotifications />
+        <FieldsFiles />
 
-      <div>
-        <AppButton class="tw-mt-6" type="submit">Отправить</AppButton>
-      </div>
+        <div>
+          <AppButton class="tw-mt-6" type="submit">Отправить</AppButton>
+        </div>
+      </form>
     </Form>
   </div>
 </template>
@@ -51,9 +53,21 @@
   ];
 
   const formRef: Ref<any> = ref(null);
-  provide('FormContext', formRef)
+  provide('FormContext', formRef);
 
-  const onSubmit = (values: object) => {
-    console.log(values);
+  function scrollToError(errors: object[]) {
+    const el = document.getElementById(Object.keys(errors)[0]);
+    const div =  el?.closest('div');
+    const label = div?.querySelector('label');
+    label?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  const onSubmit = async (validate: any) => {
+    const res = await validate();
+    if(!res.valid) {
+      scrollToError(res.errors);
+    } else {
+      console.log(res);
+    }
   }
 </script>
